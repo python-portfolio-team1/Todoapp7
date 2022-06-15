070037f30250af87e6f17881f9e1452d7f6cb3ce
Dumebiokafor <dumebiokafor20@gmail.com>
2022-06-15 13:33:00 +0100

    category added

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-from django.utils import timezone
 class Category(models.Model): # The Category table name that inherits models.Model
-    name = models.CharField(max_length=100) #Like a varchar
+    name= models.CharField(max_length=100) #Like a varchar
     class Meta:
         verbose_name = ("Category")
         verbose_name_plural = ("Categories")
@@ -14,10 +13,11 @@ class Category(models.Model): # The Category table name that inherits models.Mod
 class ToDo(models.Model):
     Title = models.CharField(max_length=100, blank=False)
     Description = models.TextField(blank = False)
-    Date = models.DateTimeField(default=timezone.now().strftime("%Y-%m-%d"), blank=False)
+    Date = models.DateTimeField(blank=False)
     Category = models.ForeignKey(Category, default="general",on_delete=models.PROTECT)
     Completed = models.BooleanField(default=False)
+    class Meta:
+        ordering = ["-Date"]
 
     def __str__(self):
         return self.Title
-      
